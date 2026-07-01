e62dbe20cab8976ffd981fa17f8e300d5d443b66
shf-ns <3104467468@qq.com>
2026-07-01 13:00:39 +0800

    refactor(dependencies): 移除未使用的 vue-router 依赖
    移除了 package.json 中未使用的 vue-router 依赖，保持项目依赖的整洁性。

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { Header, Sidebar } from '@/layout'
+
 </script>
 
 <template>
-  <RouterView />
+  <div class="app">
+    <Header />
+    <div class="main">
+      <Sidebar />
+      <div class="content">内容</div>
+      <Sidebar />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+.main {
+  flex: 1;
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+.content {
+  flex: 1;
+}
+</style>
